68094c40b53045af0311af3fdba6fc3c52a35022
Francesca993 <montinifrancesca993@gmail.com>
2025-06-07 08:19:59 +0200

    aggiunta find_quotes

--- parser/quote_handling.c
+++ parser/quote_handling.c
@@ -6,7 +6,7 @@
 /*   By: francesca <francesca@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/07 07:43:42 by francesca         #+#    #+#             */
-/*   Updated: 2025/06/07 08:08:25 by francesca        ###   ########.fr       */
+/*   Updated: 2025/06/07 08:19:04 by francesca        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,17 @@
 void find_quotes(t_pipeline *pipeline)
 {
     int i;
-    i = 0;
 
+    i = 0;
     while (i < pipeline->n_cmds)
     {
-        
+
+    if (pipeline->cmds[i]->quote_single)
+        expand_single_quotes(pipeline->cmds[i]);   // espande come literal
+
+    if (pipeline->cmds[i]->quote_double)
+        expand_double_quotes(pipeline->cmds[i]);  // espande variabili, backslash ecc.
+    
+    i++;
     }
 }
