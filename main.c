c898b35e9aacc3bdda863e0d6e621276827e9faf
Francesca993 <montinifracesca993@gmail.com>
2025-07-04 17:54:42 +0200

    format main.c

--- main.c
+++ main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: skayed <skayed@student.42roma.it>          +#+  +:+       +#+        */
+/*   By: fmontini <fmontini@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/18 13:58:21 by francesca         #+#    #+#             */
-/*   Updated: 2025/07/04 17:12:18 by skayed           ###   ########.fr       */
+/*   Updated: 2025/07/04 17:54:18 by fmontini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,18 @@
 ** Variabile globale usata per memorizzare lo stato di uscita della shell.
 ** - Aggiornata dopo l'esecuzione di ogni comando (per supportare $?)
 **
-	- Modificata anche dai signal handler per riflettere l'interruzione da segnali (es. Ctrl-C → 130)
+	- Modificata anche dai signal handler per riflettere l'interruzione 
+	da segnali (es. Ctrl-C → 130)
 **
 ** Conforme al subject:
 ** - È l'unica variabile globale usata per i segnali
 ** - Non accede né fornisce accesso ad altre strutture dati
 ** - Di tipo sig_atomic_t per garantire scrittura sicura nei signal handler
 **
-	- È lo stesso valore che l'utente potrà recuperare con $? in uno script o dopo un comando.
+	- È lo stesso valore che l'utente potrà recuperare 
+	con $? in uno script o dopo un comando.
 */
-//Dichiarazione globale (in un .c file, NON in un header .h per rispetto alla Norm)
+
 volatile sig_atomic_t	g_exit_status = 0;
 
 void	minishell_loop(char ***env)
@@ -40,7 +42,6 @@ void	minishell_loop(char ***env)
 	while (processing == 1)
 	{
 		line = readline("minishell$: ");
-		// Ctrl-D premuto
 		if (!line)
 		{
 			exit_shell(0, NULL);
@@ -50,25 +51,18 @@ void	minishell_loop(char ***env)
 		if (*line == '\0')
 		{
 			free(line);
-			continue ; // prompt nuovo senza crash
+			continue ;
 		}
 		if (*line)
 			add_history(line);
-		// ⬇️ PARSING
 		pipeline = parse_line(line, *env, pipeline);
 		if (!pipeline)
 		{
-			// lexer ha già stampato l'errore, salta solo l'esecuzione
-			// cursor dice che qui si rischia un doppio free
-			//free_pipeline(pipeline);
 			free(line);
 			continue ;
 		}
 		if (pipeline && pipeline->cmds[0] != NULL)
-		{
-			// ⬇️ ESECUZIONE
 			processing = process_pipeline(pipeline, env);
-		}
 		if (pipeline)
 			free_pipeline(pipeline);
 		free(line);
@@ -77,19 +71,16 @@ void	minishell_loop(char ***env)
 
 int	main(int argc, char **argv, char **envp)
 {
-	//so che esistono ma al momento non mi servono
+	char	**my_env;
+
 	(void)argc;
 	(void)argv;
-
-	char **my_env;
-	if(isatty(STDIN_FILENO))
+	if (isatty(STDIN_FILENO))
 		init_signals();
-
 	my_env = copy_env(envp);
 	minishell_loop(&my_env);
 	free_myenvp(my_env);
-	//rl_clear_history(); // libera la history ma lo fa gia exitshell per linux
+//	rl_clear_history();
 	clear_history();
-	//return (0);
 	return (g_exit_status);
 }
