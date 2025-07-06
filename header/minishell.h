2492e1d64cd5e3d72252fb8eda4a499f6a525229
Francesca993 <montinifrancesca993@gmail.com>
2025-07-06 13:44:13 +0200

    sistemato minishell.h

--- header/minishell.h
+++ header/minishell.h
@@ -6,7 +6,7 @@
 /*   By: francesca <francesca@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/18 13:38:15 by francesca         #+#    #+#             */
-/*   Updated: 2025/07/06 11:24:20 by francesca        ###   ########.fr       */
+/*   Updated: 2025/07/06 13:43:35 by francesca        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,24 @@
 # define MINISHELL_H
 
 # include "../Libft/libft.h"
-# include "builtins.h"
-# include "envp.h"
-# include "execution.h"
-# include "parser.h"
-# include <dirent.h>
-# include <fcntl.h>
-# include <readline/history.h>
-# include <readline/readline.h>
-# include <signal.h>
 # include <stdio.h>
 # include <stdlib.h>
-# include <string.h>
-# include <sys/stat.h>
-# include <sys/wait.h>
-# include <termcap.h>
-# include <termios.h>
 # include <unistd.h>
+# include <fcntl.h>
+# include <sys/wait.h>
+# include <signal.h>
+# include <sys/stat.h>
+# include <dirent.h>
+# include <string.h>
+# include <termios.h>
+# include <termcap.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <readline/readline.h>
+# include "envp.h"
+# include "parser.h"
+# include "execution.h"
+# include "builtins.h"
 
 extern volatile sig_atomic_t	g_exit_status;
 
